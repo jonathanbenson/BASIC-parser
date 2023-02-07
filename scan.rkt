c85f6f9931db083c80ca40760dd7f46d705208ba
Jonathan Benson <jbensonamdg@gmail.com>
2023-02-07 16:34:18 -0600

    Production code for get-token function

--- scan.rkt
+++ scan.rkt
@@ -26,7 +26,13 @@
 ; RETURN -> "return"
 ; WS -> whitespace
 
-(define (get-token char-stream token-matchers) #t)
+(define (get-token char-stream token-matchers)
+    (if (empty? token-matchers)
+        #f
+        (let ([match-result ((first token-matchers) char-stream)])
+            (if (and (eq? match-result #f))
+                (get-token char-stream (rest token-matchers))
+                match-result))))
 
 (define (scan char-stream token-matchers [token-stream (list)])
     (let ([next-token (get-token char-stream token-matchers)])
