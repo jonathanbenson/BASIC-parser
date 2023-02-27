da550c069d4c4504db98d94a8be986994e9715f0
Jonathan Benson <jbensonamdg@gmail.com>
2023-02-27 16:22:11 -0600

    Production code written for linelist, line, idx functions

--- src/parser.rkt
+++ src/parser.rkt
@@ -5,5 +5,50 @@
         #t
         #f))
 
+(define (match-any-token expected-token-types token-stream)
+    (cond
+        [(empty? expected-token-types) #f]
+        [(eq? (first expected-token-types) (first (first token-stream))) #t]
+        [else (match-any-token (rest expected-token-types) token-stream)]))
+
+(define (syntax-error line-number)
+    (string-append "Syntax error on line " (number->string line-number) "."))
+
+(define (program token-stream [line-number 1])
+    (cond
+        [(match-token 'NONZERO-DIGIT token-stream) (linelist (rest token-stream) line-number)]
+        [(match-token 'EOP token-stream) "Accept"]
+        [else (syntax-error line-number)]))
+
+(define (linelist token-stream line-number)
+    (cond
+        [(match-token 'NONZERO-DIGIT token-stream) (line (rest token-stream) line-number)]
+        [(match-token 'EOP token-stream) (program token-stream line-number)]
+        [else (syntax-error line-number)]))
+
+(define (line token-stream line-number)
+    (cond
+        [(match-token 'NONZERO-DIGIT token-stream) (idx (rest token-stream) line-number line)]
+        ;;; [(match-any-token (list 'ID 'IF 'READ 'WRITE 'GOTO 'GOSUB 'RETURN)) (stmt token-stream line-number)]
+        ;;; [(match-token 'COLON token-stream) (linetail token-stream line-number)]
+        [(match-token 'EOL token-stream) (linelist token-stream (+ line-number 1))]
+        [else (syntax-error line-number)]))
+
+(define (idx token-stream line-number parent-callback)
+    (cond
+        [(match-token 'NONZERO-DIGIT token-stream) (idx (rest token-stream) line-number)]
+        [else (parent-callback (rest token-stream) line-number)]))
+
+;;; (define (expr token-stream line-number parent-callback)
+;;;     )
+
+;;; (define (stmt token-stream line-number)
+;;;     (cond
+;;;         [(and (match-token 'READ token-stream) (match-token 'ID (rest token-stream))) (linetail (rest (rest token-stream)) line-number)]
+;;;         [(and (match-token 'READ token-stream) (match-token 'ID (rest token-stream))) (linetail (rest (rest token-stream)) line-number)]))
+
 (provide
-    match-token)
+    match-token
+    match-any-token
+    syntax-error
+    program)
--- test/parser.test.rkt
+++ test/parser.test.rkt
@@ -2,7 +2,7 @@
 
 (require rackunit "../src/parser.rkt")
 
-; test match function
+; test match-token function
 
 (define check-token-stream
     '(
@@ -25,4 +25,16 @@
 (check-equal? (match-token 'NONZERO-DIGIT check-token-stream) #t)
 (check-equal? (match-token 'ZERO-DIGIT check-token-stream) #f)
 
+; test match-any-token
+(check-equal? (match-any-token (list 'NONZERO-DIGIT 'ZERO-DIGIT 'GOTO) check-token-stream) #t)
+(check-equal? (match-any-token (list 'ZERO-DIGIT 'GOTO 'NONZERO-DIGIT) check-token-stream) #t)
+(check-equal? (match-any-token (list 'ZERO-DIGIT 'GOTO) check-token-stream) #f)
 
+; test syntax-error function
+
+(check-equal? (syntax-error 1) "Syntax error on line 1.")
+(check-equal? (syntax-error 5) "Syntax error on line 5.")
+
+; test program function
+(check-equal? (program '((EOP "$$"))) "Accept")
+(check-equal? (program '((ZERO-DIGIT "0"))) "Syntax error on line 1.")
