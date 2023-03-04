1ac0167d13f0d50e288ddd94938ce16d79673b0a
Jonathan Benson <jbensonamdg@gmail.com>
2023-03-04 11:38:38 -0600

    Refactoring

--- src/parser.rkt
+++ src/parser.rkt
@@ -20,20 +20,32 @@
 (define (syntax-error line-number debug-msg last-token)
     (string-append "Syntax error on line " (number->string line-number) ". Debug message: " debug-msg "... Last token: " (symbol->string last-token)))
 
-(define (match-many-nonterminals nonterminal-matchers token-stream)
-    (if (empty? nonterminal-matchers)
+(define (match-many matchers token-stream)
+    (if (empty? matchers)
         (list #t token-stream)
-        (let ([match-result ((first nonterminal-matchers) token-stream)])
+        (let ([match-result ((first matchers) token-stream)])
             (if (first match-result)
-                (match-many-nonterminals (rest nonterminal-matchers) (second match-result))
+                (match-many (rest matchers) (second match-result))
                 (list #f token-stream)))))
 
 (define (match-num token-stream [found-numsign? #f] [found-digit? #f])
     (cond
-        [(empty? token-stream) (if found-digit? (list #t token-stream) (list #f token-stream))]
         [(match-any-token '(PLUS MINUS) token-stream) (if found-digit? (list #f (rest token-stream)) (match-num (rest token-stream) #t found-digit?))]
         [(match-any-token '(NONZERO-DIGIT ZERO-DIGIT) token-stream) (match-num (rest token-stream) found-numsign? #t)]
         [else (if found-digit? (list #t token-stream) (list #f token-stream))]))
+
+;;; (define (match-expr token-stream)
+;;;     (let ([match-num-result (match-num token-stream)])
+;;;         (cond
+;;;             [(match-token 'ID token-stream) (match-etail (rest token-stream))]
+;;;             [(first match-num-result) (match-etail (second match-num-result))]
+;;;             [(match-many)]
+
+;;; (define (match-etail token-stream)
+;;;     (cond
+;;;         [(match-match-any-token '(PLUS MINUS ASSIGN-OP) token-stream) (match-expr (rest token-stream))]
+;;;         [else (list #t token-stream)]))
+
 
 
 (provide
